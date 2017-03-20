7978750f7bd34169d0dd8cc4bac21382a0ca922b
PC-01\Atwind <atwind@cszi.com>
2017-03-20 16:41:13 +0800

    修正BUG

--- sources/CS.CodeRobot/CS.CodeRobot.Generator/JsGenerators/JsGenerator.cs
+++ sources/CS.CodeRobot/CS.CodeRobot.Generator/JsGenerators/JsGenerator.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.CodeDom.Compiler;
 using System.IO;
+using System.Reflection;
+using CS.Extension;
 using CS.Logging;
+using CS.Utils;
 using Microsoft.JScript;
 
 namespace CS.CodeRobot.JsGenerators
@@ -48,8 +51,11 @@ namespace CS.CodeRobot.JsGenerators
                 GenerateInMemory = true
             };
             //opt.ReferencedAssemblies.Add("System.dll");
-            opt.ReferencedAssemblies.Add("CS.Utility.dll");
-            opt.ReferencedAssemblies.Add("CS.CodeRobot.Generator.dll");
+            //Assembly.LoadFile(FileHelper.GetFullPath("~/CS.Utility.dll"));
+            //Assembly.LoadFile(FileHelper.GetFullPath("~/CS.CodeRobot.Generator.dll"));
+            var x = "1".ToInt(0);
+            opt.ReferencedAssemblies.Add(FileHelper.GetFullPath("~/CS.Utility.dll"));
+            opt.ReferencedAssemblies.Add(FileHelper.GetFullPath("~/CS.CodeRobot.Generator.dll")); //非GAC必须全路径
             var content = File.ReadAllText(jsFilePath);
             var result = JsCompiler.CompileAssemblyFromSource(opt, content);
             if (result.Errors.Count > 0)
